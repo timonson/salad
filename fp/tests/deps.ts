ea04c05b24336d7ff42df2e86aa84116c7cf2fd9
timonson <timo.rossa@tutanota.com>
2021-10-18 00:46:31 +0200

    -

--- fp/tests/deps.ts
+++ fp/tests/deps.ts
@@ -2,7 +2,7 @@ export {
   assertEquals,
   assertThrows,
   assertThrowsAsync,
-} from "https://deno.land/std@0.97.0/testing/asserts.ts";
+} from "https://deno.land/std@0.111.0/testing/asserts.ts";
 
 export const val1 = 10;
 export const val2 = 20;
